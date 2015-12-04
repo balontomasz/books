cb421770a89e0aea1bef950ac9d077a27dadd78b
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-12-04 21:52:00 +0100

    parse xml and return some data

--- web/services/isbndb.ex
+++ web/services/isbndb.ex
@@ -2,10 +2,36 @@ defmodule Books.Isbndb do
   @access_key System.get_env("ISBNDB_ACCESS_KEY")
 
   def fetch(isbn) do
-    response = HTTPotion.get(link(isbn))
+    xml = response(isbn).body
+
+    %{
+      :author => author(xml),
+      :cover_url => cover_url(isbn),
+      :isbn => isbn,
+      :title => title(xml)
+    }
+  end
+
+  defp response(isbn) do
+    HTTPotion.get(link(isbn))
   end
 
   defp link(isbn) do
     "http://isbndb.com/api/books.xml?access_key=#{@access_key}&results=details&index1=isbn&value1=#{isbn}"
+  end
+
+  def title(xml) do
+    titlelong = Floki.FlatText.get(Floki.find(xml, "titlelong"))
+    title = Floki.FlatText.get(Floki.find(xml, "title"))
+
+    if String.length(titlelong) > 0, do: titlelong, else: title
+  end
+
+  def author(xml) do
+    Floki.FlatText.get(Floki.find(xml, "authorstext"))
+  end
+
+  defp cover_url(isbn) do
+    "http://covers.openlibrary.org/b/isbn/#{isbn}-L.jpg"
   end
 end
